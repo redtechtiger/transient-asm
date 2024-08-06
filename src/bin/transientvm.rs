ac0b88095bba866f1a731f4783bfdee5c723cd67
RedTechTiger <32938014+RedTechTiger@users.noreply.github.com>
2024-08-06 20:10:42 +0200

    Feature: Rework architecture and add support for 16 bit addresses and up to 64 bit arithmetic

--- src/bin/transientvm.rs
+++ src/bin/transientvm.rs
@@ -46,7 +46,7 @@ const HLT: u8 = 0xFF;
 
 use std::env::args;
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read,Write};
 
 const TRANSIENT_MEM_MAX: usize = 256;
 
@@ -93,110 +93,125 @@ impl<const TRANSIENT_MEM_MAX: usize> TransientState<TRANSIENT_MEM_MAX> {
         }
     }
     /// Fetches an instruction at the given address (mind the alignment!)
-    pub fn resolve_instruction(&self, base: usize) -> [u8; 4] {
+    pub fn resolve_instruction(&self, base: usize) -> [u8; 8] {
         assert!(
-            self.memory.len() > base + 3,
+            self.memory.len() > base + 7,
             "Halt: Attempted instruction resolution beyond memory space"
         );
-        self.memory[base..][..4].try_into().unwrap()
+        self.memory[base..][..8].try_into().unwrap()
+    }
+    pub fn memory_write(&mut self, address: usize, data: &[u8], size: usize) {
+        assert!(
+            self.memory.len() > address + size,
+            "Halt: Attempted memory write beyond memory scope"
+        );
+        self.memory[address..][..size].copy_from_slice(&data[data.len()-size..][..size]);
+
     }
     /// Executes an instruction and returns the next program counter
-    pub fn execute_instruction(&mut self, instruction: [u8; 4]) -> usize {
+    pub fn execute_instruction(&mut self, instruction: [u8; 8]) -> usize {
         // Decodes instruction
         let opcode = instruction[0];
-        let source1 = instruction[1] as usize;
-        let source2 = instruction[2] as usize;
-        let destination = instruction[3] as usize;
-
+        let size = instruction[1] as usize;
+        let source1_address = i16::from_be_bytes([instruction[2], instruction[3]]) as usize;
+        let source2_address = i16::from_be_bytes([instruction[4], instruction[5]]) as usize; 
+        let destination_address = i16::from_be_bytes([instruction[6], instruction[7]]) as usize;
+        
         // Validates memory pointers
         assert!(
-            self.memory.len() > source1,
+            self.memory.len() > source1_address,
             "Halt: Attempt to access memory beyond memory space (source1)"
         );
         assert!(
-            self.memory.len() > source2,
+            self.memory.len() > source2_address,
             "Halt: Attempt to access memory beyond memory space (source2)"
         );
         assert!(
-            self.memory.len() > destination,
+            self.memory.len() > destination_address,
             "Halt: Attempt to access memory beyond memory space (destination)"
         );
 
+        let source1_data: &[u8] = &self.memory[source1_address..][..size];
+        let source1_data: u64 = u64::from_be_bytes(u64_pad_be(source1_data));
+        
+        let source2_data: &[u8] = &self.memory[source2_address..][..size];
+        let source2_data: u64 = u64::from_be_bytes(u64_pad_be(source2_data));
+        
         match opcode {
             MOV => {
-                self.memory[destination] = self.memory[source1];
-                self.program_counter + 4
+                self.memory_write(destination_address, &source1_data.to_be_bytes(), size);
+                self.program_counter + 8
             }
             ADD => {
-                self.memory[destination] = self.memory[source1] + self.memory[source2];
-                self.program_counter + 4
+                self.memory_write(destination_address, &(source1_data.checked_add(source2_data).expect("Halt: Arithmetic overflow")).to_be_bytes(), size);
+                self.program_counter + 8
             }
             SUB => {
-                self.memory[destination] = self.memory[source1] - self.memory[source2];
-                self.program_counter + 4
+                self.memory_write(destination_address, &(source1_data.checked_sub(source2_data).expect("Halt: Arithmetic overflow")).to_be_bytes(), size);
+                self.program_counter + 8
             }
             MUL => {
-                self.memory[destination] = self.memory[source1] * self.memory[source2];
-                self.program_counter + 4
+                self.memory_write(destination_address, &(source1_data.checked_mul(source2_data).expect("Halt: Arithmetic overflow")).to_be_bytes(), size);
+                self.program_counter + 8
             }
             DIV_T => {
-                self.memory[destination] = self.memory[source1] / self.memory[source2];
-                self.program_counter + 4
+                self.memory_write(destination_address, &(source1_data.checked_div(source2_data).expect("Halt: Arithmetic overflow")).to_be_bytes(), size);
+                self.program_counter + 8
             }
             DIV_R => {
-                self.memory[destination] =
-                    (self.memory[source1] as f64 / self.memory[source2] as f64) as u8;
-                self.program_counter + 4
+                self.memory_write(destination_address, &
+                    ((source1_data as f64 / source2_data as f64) as u64).to_be_bytes(), size);
+                self.program_counter + 8
             }
             REM => {
-                self.memory[destination] = (self.memory[source1]) % self.memory[source2];
-                self.program_counter + 4
+                self.memory_write(destination_address, &(source1_data % source2_data).to_be_bytes(), size);
+                self.program_counter + 8
             }
             CGT => {
-                self.memory[destination] = (self.memory[source1] > self.memory[source2]) as u8;
-                self.program_counter + 4
+                self.memory_write(destination_address, &((source1_data > source2_data) as u64).to_be_bytes(), size);
+                self.program_counter + 8
             }
             CLT => {
-                self.memory[destination] = (self.memory[source1] < self.memory[source2]) as u8;
-                self.program_counter + 4
+                self.memory_write(destination_address, &((source1_data < source2_data) as u64).to_be_bytes(), size);
+                self.program_counter + 8
             }
             #[rustfmt::skip]
             JMP => {
-                source1
+                source1_address
             },
             JIE => {
-                if self.memory[source2] == 0 {
+                if source2_data == 0 {
                     // Zero
-                    self.program_counter + 4
+                    self.program_counter + 8
                 } else {
                     // Nonzero
-                    source1
+                    source1_address
                 }
             }
             JNE => {
-                if self.memory[source2] == 0 {
+                if source2_data == 0 {
                     // Zero
-                    source1
+                    source1_address
                 } else {
                     // Nonzero
-                    self.program_counter + 4
+                    self.program_counter + 8
                 }
             }
             PUT_I => {
-                print!("{}", self.memory[source1]);
-                self.program_counter + 4
+                print!("{}", source1_data);
+                self.program_counter + 8
             }
             PUT_C => {
-                print!("{}", char::from(self.memory[source1]));
-                self.program_counter + 4
+                print!("{}", char::from(source1_data as u8));
+                self.program_counter + 8
             }
             IMZ => {
-                self.memory[destination] = self.image_length as u8;
-                self.program_counter + 4
+                self.memory_write(destination_address, &(self.image_length as u64).to_be_bytes(), size);
+                self.program_counter + 8
             }
             EQU => {
-                self.memory[destination] = (source1 == source2) as u8;
-                self.program_counter + 4
+                self.memory_write(destination_address, &((source1_data == source2_data) as u64).to_be_bytes(), size);
+                self.program_counter + 8
             }
             HLT => {
                 self.mode = TransientMode::HALTED;
@@ -204,15 +219,39 @@ impl<const TRANSIENT_MEM_MAX: usize> TransientState<TRANSIENT_MEM_MAX> {
             }
             _ => {
                 panic!(
-                    "Halt: Unsupported opcode! Instruction: 0x{:0>2x}{:0>2x}{:0>2x}{:0>2x}",
-                    opcode, source1, source2, destination
+                    "Halt: Unsupported opcode! Instruction: 0x{:0>2x}{:0>4x}{:0>4x}{:0>4x}",
+                    opcode, source1_address, source2_address, destination_address
                 );
             }
         }
     }
 }
 
+fn u64_pad_be(data: &[u8]) -> [u8; 8] {
+    let mut padded = [0u8; 8];
+    padded[(8 - data.len())..].copy_from_slice(data);
+    padded
+}
+
 fn main() {
+    // DEBUG: Write a fibonacci program
+    let _ROM: [u8; 80] = [
+    //  OPCO, SIZE, SRC1, SRC1, SRC2, SRC2, DEST, DEST
+        0x0D, 0x08, 0x00, 0064, 0x00, 0x00, 0x00, 0x00,
+        0x0E, 0x08, 0x00, 0072, 0x00, 0x00, 0x00, 0x00,
+        0x02, 0x08, 0x00, 0048, 0x00, 0056, 0x00, 0064,
+        0x01, 0x08, 0x00, 0056, 0x00, 0x00, 0x00, 0048,
+        0x01, 0x08, 0x00, 0064, 0x00, 0x00, 0x00, 0056,
+        0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0A,
+    ];
+    let mut one_plus_two = File::create("yay-2.0.bin").unwrap();
+    one_plus_two.write(&_ROM).unwrap();
+    one_plus_two.flush().unwrap();
+
     // Verify input arguments
     let args: Vec<String> = args().collect();
     if args.len() != 2 {
